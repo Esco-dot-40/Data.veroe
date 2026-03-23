a0ed877b6cd1a0b59c3d762430458a04b5532f90
Veroe Admin <voee1@veroe.space>
2026-03-22 20:22:27 -0400

    chore: strictly sanitize node deployment by purging all diagnostic scripts and enforcing hard gitignores against workspace leaks

--- fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('index.js', 'utf8');
-
-code = code.replace(/\\`/g, '`');
-code = code.replace(/\\\$\{/g, '${');
-
-fs.writeFileSync('index.js', code);
--- fix_html.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('index.html', 'utf8');
-code = code.replace(/\\`/g, '`');
-code = code.replace(/\\\$\{/g, '${');
-fs.writeFileSync('index.html', code);
